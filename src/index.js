bfda53c07ac626d83dd1cc99cb56c61160497f30
MUN SEONG UK <gukbab1216@gmail.com>
2018-09-17 20:22:56 +0900

    타이틀 및 index 업데이트

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+//import { createBrowserHistory } from "history";
+//import { Router, Route, Switch } from "react-router-dom";
 
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "assets/scss/now-ui-dashboard.css?v=1.2.0";
 import "assets/css/demo.css";
 
-import indexRoutes from "routes/index.jsx";
+//import indexRoutes from "routes/index.jsx";
 
-const hist = createBrowserHistory();
+//const hist = createBrowserHistory();
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>Hello React</div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+  <Main />,
+  document.getElementById('root')
 );
